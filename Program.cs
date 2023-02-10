88c25bc7552db5b3f806730d2266e4f686140038
Cemtug Kaan Tasan <cemtugtasanedding@gmail.com>
2023-02-10 17:19:35 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmIReal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasaDon:
+            Kisi kisi = new Kisi();
+
+            Console.WriteLine("Lütfen Adınızı Giriniz");
+            kisi.Ad = Console.ReadLine();
+            
+            Console.WriteLine("Lütfen Soyad Giriniz");
+            kisi.Soyad = Console.ReadLine();
+            
+            Console.WriteLine("Lütfen Doğum Yılı Giriniz");
+            kisi.DogumYili =Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Lütfen TcNo Giriniz");
+            kisi.TcNo = long.Parse(Console.ReadLine());
+
+            bool durum;
+            try
+            {
+                using (KimlikDogrula.KPSPublicSoapClient service = new KimlikDogrula.KPSPublicSoapClient { }) 
+                { 
+                    durum = service.TCKimlikNoDogrula(kisi.TcNo, kisi.Ad, kisi.Soyad,kisi.DogumYili);                    
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (durum == true)
+            {
+                Console.WriteLine("Konsola girmis oldugunuz bilgiler DOGRUDUR.");
+            }
+            else
+            {
+                Console.WriteLine("Konsola girmis oldugunuz bilgiler YANLISTIR");
+            }
+            goto BasaDon;
+        }
+        class Kisi 
+        { 
+            public string Ad { get; set; }
+            public string Soyad { get; set; }
+            public int DogumYili { get; set; }
+            public long TcNo { get; set; }    
+        }
+
+    }
+}
